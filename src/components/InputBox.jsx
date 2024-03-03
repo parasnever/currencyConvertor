a0aeed0a0db7f1891cce293ffd44034088d8e526
paras from office <parasshresthanever@gmail.com>
2024-03-03 15:02:56 -0800

    code

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, {useId} from 'react'
 
 function InputBox({
   label,
   amount,
   onAmountChange,
   onCurrencychange,
-  currencyOption = [],
+  currencyOptions = [],
   selectCurrency = "usd",
   amountDisable = false,
   currencyDisable = false,
@@ -13,15 +13,16 @@ function InputBox({
   className = "",
 }) {
  
+    const amountInputId = useId()
 
   return (
       <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
           <div className="w-1/2">
-              <label  className="text-black/40 mb-2 inline-block">
+              <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
               {label}
               </label>
               <input
-                  
+                  id={amountInputId}
                   className="outline-none w-full bg-transparent py-1.5"
                   type="number"
                   placeholder="Amount"
@@ -34,12 +35,19 @@ function InputBox({
               <p className="text-black/40 mb-2 w-full">Currency Type</p>
               <select
                   className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                  value={selectCurrency}
+                  onChange={(e)=> onCurrencychange && onCurrencychange(e.target.value)}
+                  disabled={currencyDisable}
                   
               >
-                  
-                      <option value="usd">
-                          usd
-                      </option>
+                {
+                    currencyOptions.map((currency)=>(
+                        <option key={currency} value={currency}>
+                        {currency}
+                    </option>
+                    ))
+                }
+                    
               
               </select>
           </div>
