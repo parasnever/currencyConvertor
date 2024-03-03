20f68cd9e0638687c8609380961ce76be4ba3146
paras from office <parasshresthanever@gmail.com>
2024-03-03 15:58:32 -0800

    this is

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,17 +1,16 @@
-import { useEffect , useState } from "react";
-  
+import {useEffect, useState} from "react"
+
 
 function useCurrencyInfo(currency){
-    const [data , setData ] = useState({})
-    useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-        )
-        .then((res)=>res.json())
-        .then((res)=> setData(res[currency]))
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
         console.log(data);
-    },[currency])
+    }, [currency])
     console.log(data);
     return data
-
 }
-export default useCurrencyInfo
+
+export default useCurrencyInfo;
